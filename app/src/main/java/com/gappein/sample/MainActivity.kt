9c46b34c3ae0f23694ea2dce1523aa86f0b9bc89
Himanshu Singh <hello2himanshusingh@gmail.com>
2020-10-07 21:09:25 +0530

    Sticker View

--- app/src/main/java/com/gappein/sample/MainActivity.kt
+++ app/src/main/java/com/gappein/sample/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.gappein.sample
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.gappein.StickerView
@@ -11,8 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val x = StickerView(this)
-        x.updateText("dsfsdfsdf")
         setContentView(R.layout.activity_main)
-        background.setImageBitmap(x.generateSticker())
+        background.setImageBitmap(x.generateSticker("Himanshu"))
     }
 }
--- sticker/src/main/java/com/gappein/util/StickerExtension.kt
+++ sticker/src/main/java/com/gappein/util/StickerExtension.kt
@@ -2,12 +2,12 @@ package com.gappein.util
 
 import android.graphics.Bitmap
 import android.graphics.Canvas
-import android.util.Log
 import android.view.View.MeasureSpec
 import com.gappein.StickerView
 
-fun StickerView.generateSticker(): Bitmap {
+fun StickerView.generateSticker(text:String): Bitmap {
     val view = this
+    view.updateText(text)
     view.measure(MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED)
     val bitmap = Bitmap.createBitmap(
         view.measuredWidth, view.measuredHeight,
